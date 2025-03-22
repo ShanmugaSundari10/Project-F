d2d4fc2afc81fab302fcda6b27e3b1faed20f20f
ShanmugaSundari10 <suhapalani25@gmail.com>
2025-03-22 17:13:15 +0530

    Adding database

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCMollyFH_iyh-Tpg5ewzp6XLjJG7wirLw",
+  authDomain: "form-fire-7de95.firebaseapp.com",
+  projectId: "form-fire-7de95",
+  storageBucket: "form-fire-7de95.firebasestorage.app",
+  messagingSenderId: "521231638434",
+  appId: "1:521231638434:web:b8b8083cf45df44acfdcfb"
+};
+
+// Initialize Firebase
+const apps = initializeApp(firebaseConfig);
 // Import Firebase modules (if using Firebase SDK 9 or above)
 import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, set, get, child } from 'firebase/database';
@@ -5,8 +22,8 @@ const appSettings = {
     databaseURL: "https://form-fire-7de95-default-rtdb.firebaseio.com/",
 };
 // Initialize Firebase with your config
-const app = initializeApp(appSettings);
-const database = getDatabase(app);
+// const app = initializeApp(appSettings);
+const database = getDatabase(apps);
 const userListInDB = ref(database, "users");
 
 const idEl = document.querySelector("#id");
